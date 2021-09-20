a0c696144864cf7da93ec8434fe246338df3c648
Jorge <66280538+jorgeholberton@users.noreply.github.com>
2021-09-19 22:30:50 -0500

    Actualización del Select
    Se encuentra funcionando  el select de los sistemas de medición, métrico e ingles.

--- javascript/formulario_imc.js
+++ javascript/formulario_imc.js
@@ -4,10 +4,44 @@ document.addEventListener
       document.getElementById("registro").addEventListener('submit', validarFormulario); 
    }
  );
- 
+
+
+var combo=1;
+function metricas() 
+{
+    
+   combo=document.getElementById('combo').value;
+   
+    if(combo==1)
+    {
+      
+      document.getElementById('lbl_est').innerHTML='<strong>Estatura en Metros:</strong> <abbr title="Obligatorio" aria-label="required">*</abbr>';
+       document.getElementById('lbl_peso').innerHTML='<strong>Peso en Kilos:</strong> <abbr title="Obligatorio" aria-label="required">*</abbr>';
+       
+    }
+    else
+    {
+     
+      document.getElementById('lbl_est').innerHTML='<strong>Estatura en Pulgadas:</strong> <abbr title="Obligatorio" aria-label="required">*</abbr>';
+       document.getElementById('lbl_peso').innerHTML='<strong>Peso en Libras:</strong> <abbr title="Obligatorio" aria-label="required">*</abbr>';
+       
+    }
+}
+
+
+
+
+
+
+
  function validarFormulario(evento) 
  {
    evento.preventDefault();
+   //combo=document.getElementById('combo').value;
+ ////////////////
+ if(combo==1)//Métrico
+ {
+   
    var est = document.getElementById('est').value;
    var peso = document.getElementById('peso').value;
   
@@ -22,7 +56,7 @@ document.addEventListener
      alert('Campo Peso Vacio');
      return;
    }
-
+   
    var cat_normal='Normal';
    var cat_bajopeso='Bajo Peso';
    var cat_sobrepeso='Sobre Peso';
@@ -31,7 +65,7 @@ document.addEventListener
    var normal = 'Su peso está en la categoría Normal para adultos de su misma estatura. Para su estatura, un peso normal variaría entre 53.5  a  72 kilogramos. Mantener un peso saludable puede reducir el riesgo de enfermedades crónicas asociadas al sobrepeso y la obesidad.';
    var bajopeso = 'Su peso está en la categoría de Bajo peso para adultos de su misma estatura. Para su estatura, un peso normal variaría entre 53.5  a  72 kilogramos. Hable con su proveedor de atención médica para establecer las posibles causas del bajo peso y si necesita ganar peso. ';
    var sobrepeso = 'Su peso está en la categoría de Sobrepeso para adultos de su misma estatura. Para su estatura, un peso normal variaría entre 49.2 a 66.2 kilogramos. Las personas que tienen sobrepeso o son obesas tienen un mayor riesgo de afecciones crónicas, tales como hipertensión arterial, diabetes y colesterol alto.     Toda persona que tenga sobrepeso debería tratar de evitar ganar más peso. Además, si usted tiene sobrepeso junto con otros factores de riesgo (como niveles altos de colesterol LDL, niveles bajos de colesterol HDL o hipertensión arterial), debería tratar de perder peso. Incluso una pequeña disminución (tan solo 10 % de su peso actual) puede ayudar a disminuir el riesgo de enfermedades. Hable con su proveedor de atención médica para establecer maneras adecuadas de perder peso.';
-   var obeso = 'Su peso está en la categoría de Obeso para adultos de su misma estatura. Para su estatura, un peso normal variaría entre 47.4 a 63.7 kilogramos. Las personas que tienen sobrepeso o son obesas tienen un mayor riesgo de afecciones crónicas, tales como hipertensión arterial, diabetes y colesterol alto. Toda persona que tenga sobrepeso debería tratar de evitar ganar más peso. Además, si usted tiene sobrepeso junto con otros factores de riesgo (como niveles altos de colesterol LDL, niveles bajos de colesterol HDL o hipertensión arterial), debería tratar de perder peso. Incluso una pequeña disminución (tan solo 10 % de su peso actual) puede ayudar a disminuir el riesgo de enfermedades. Hable con su proveedor de atención médica para establecer maneras adecuadas de perder peso.';
+   var obeso = '1Su peso está en la categoría de Obeso para adultos de su misma estatura. Para su estatura, un peso normal variaría entre 47.4 a 63.7 kilogramos. Las personas que tienen sobrepeso o son obesas tienen un mayor riesgo de afecciones crónicas, tales como hipertensión arterial, diabetes y colesterol alto. Toda persona que tenga sobrepeso debería tratar de evitar ganar más peso. Además, si usted tiene sobrepeso junto con otros factores de riesgo (como niveles altos de colesterol LDL, niveles bajos de colesterol HDL o hipertensión arterial), debería tratar de perder peso. Incluso una pequeña disminución (tan solo 10 % de su peso actual) puede ayudar a disminuir el riesgo de enfermedades. Hable con su proveedor de atención médica para establecer maneras adecuadas de perder peso.';
    
               
             var masa = (peso / Math.pow(est, 2)).toFixed(2);//dos posiciones decimales
@@ -61,12 +95,25 @@ document.addEventListener
                   document.registro.msg.value = bajopeso;
                   document.registro.categoria_obtenida.value = cat_bajopeso;
                }
-   //this.submit();
- }
+  }
+  else //Inglés
+  {
+   var est = document.getElementById('est').value;
+   var peso = document.getElementById('peso').value;
+   
+  
+   if(est.length == 0) 
+   {
+     alert('Campo Estatura Vacio');
+     return;
+   }
 
-/*
-function obtenerDatos() 
-{
+   if (peso.length == 0) 
+   {
+     alert('Campo Peso Vacio');
+     return;
+   }
+   
    var cat_normal='Normal';
    var cat_bajopeso='Bajo Peso';
    var cat_sobrepeso='Sobre Peso';
@@ -75,13 +122,10 @@ function obtenerDatos()
    var normal = 'Su peso está en la categoría Normal para adultos de su misma estatura. Para su estatura, un peso normal variaría entre 53.5  a  72 kilogramos. Mantener un peso saludable puede reducir el riesgo de enfermedades crónicas asociadas al sobrepeso y la obesidad.';
    var bajopeso = 'Su peso está en la categoría de Bajo peso para adultos de su misma estatura. Para su estatura, un peso normal variaría entre 53.5  a  72 kilogramos. Hable con su proveedor de atención médica para establecer las posibles causas del bajo peso y si necesita ganar peso. ';
    var sobrepeso = 'Su peso está en la categoría de Sobrepeso para adultos de su misma estatura. Para su estatura, un peso normal variaría entre 49.2 a 66.2 kilogramos. Las personas que tienen sobrepeso o son obesas tienen un mayor riesgo de afecciones crónicas, tales como hipertensión arterial, diabetes y colesterol alto.     Toda persona que tenga sobrepeso debería tratar de evitar ganar más peso. Además, si usted tiene sobrepeso junto con otros factores de riesgo (como niveles altos de colesterol LDL, niveles bajos de colesterol HDL o hipertensión arterial), debería tratar de perder peso. Incluso una pequeña disminución (tan solo 10 % de su peso actual) puede ayudar a disminuir el riesgo de enfermedades. Hable con su proveedor de atención médica para establecer maneras adecuadas de perder peso.';
-   var obeso = 'Su peso está en la categoría de Obeso para adultos de su misma estatura. Para su estatura, un peso normal variaría entre 47.4 a 63.7 kilogramos. Las personas que tienen sobrepeso o son obesas tienen un mayor riesgo de afecciones crónicas, tales como hipertensión arterial, diabetes y colesterol alto. Toda persona que tenga sobrepeso debería tratar de evitar ganar más peso. Además, si usted tiene sobrepeso junto con otros factores de riesgo (como niveles altos de colesterol LDL, niveles bajos de colesterol HDL o hipertensión arterial), debería tratar de perder peso. Incluso una pequeña disminución (tan solo 10 % de su peso actual) puede ayudar a disminuir el riesgo de enfermedades. Hable con su proveedor de atención médica para establecer maneras adecuadas de perder peso.';
+   var obeso = '2Su peso está en la categoría de Obeso para adultos de su misma estatura. Para su estatura, un peso normal variaría entre 47.4 a 63.7 kilogramos. Las personas que tienen sobrepeso o son obesas tienen un mayor riesgo de afecciones crónicas, tales como hipertensión arterial, diabetes y colesterol alto. Toda persona que tenga sobrepeso debería tratar de evitar ganar más peso. Además, si usted tiene sobrepeso junto con otros factores de riesgo (como niveles altos de colesterol LDL, niveles bajos de colesterol HDL o hipertensión arterial), debería tratar de perder peso. Incluso una pequeña disminución (tan solo 10 % de su peso actual) puede ayudar a disminuir el riesgo de enfermedades. Hable con su proveedor de atención médica para establecer maneras adecuadas de perder peso.';
    
-   var est = document.getElementById('est').value;
-   var peso = document.getElementById('peso').value;
-   
-             
-            var masa = (peso / Math.pow(est, 2)).toFixed(2);//dos posiciones decimales
+              
+            var masa = ((peso *703)/ Math.pow(est, 2)).toFixed(2);//dos posiciones decimales
 
                document.registro.imcobtenido.value = masa;
             
@@ -108,55 +152,6 @@ function obtenerDatos()
                   document.registro.msg.value = bajopeso;
                   document.registro.categoria_obtenida.value = cat_bajopeso;
                }
-}*/
-/*
-function validarEmail(email) 
-{
-   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(valor))
-   {
-    alert("La dirección de email " + valor + " es correcta.");
-   } 
-   else 
-   {
-    alert("La dirección de email es incorrecta.");
-   }
- }*/
-
- /* 
-El email se compone de tres partes:
-
-nombre usuario + @ + servidor + dominio
-Veamos que consideraciones debemos de tener con cada una de esas partes.
-
-Nombre y servidor
-
-Debe de empezar por letra o numero. Al menos tiene una letra o número. La letra o número se expresa mediante el carácter \w. Para asegurarnos de que la letra o número aparece al menos una vez utilizaremos el modificador +.
-Puede contener puntos y guiones además de las letras y números. Esta combinación podrá aparecer, es por ello que utilizaremos el modificador * (cero o varias veces). Insertaremos toda la combinación entre paréntesis.
-Su expresión regular, para ambos casos, será la siguiente:
-
-\w+([\.-]?\w+)*
-Dominio
-
-Irá al final, detrás de un punto. Podrá tener dos (.es, .fr, .it,…) o tres letras (.com, .net, .org,..) o cuatro (.mobi, info,…). Si queremos indicar un número concreto de caracteres lo expresamos con el número entre los operadores { y }
-Además podemos tener varios dominios seguidos (.com.ar, .com.uk,….) es por ello que deberemos de usar el modificador +. Ya que el dominio podrá aparecer varias veces.
-Su expresión regular será comos sigue:
-
-(\.\w{2,3,4})+
-La expresión regular final para validar el email con JavaScript sería:
-Para la implementación de validar el email con JavaScript vamos a crear un formulario, el cual llamará a la función validarEmail. Esta función será la que ejecute la expresión regular.
-
-function validarEmail(email){...}
-La expresión regular la ejecutaremos mediante el método test del objeto RegExp. El objeto RegExp, como su nombre bien indica, representa las expresiones regulares.
-
-function validarEmail(valor) 
-{
-  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(valor))
-  {
-   alert("La dirección de email " + valor + " es correcta.");
-  } 
-  else 
-  {
-   alert("La dirección de email es incorrecta.");
   }
-}*/
-
+   //this.submit();
+ }
